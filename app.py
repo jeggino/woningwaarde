c589f199aa91ca6db8c4e93bc7685be08237d280
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-16 16:29:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -503,3 +503,45 @@ option_2 = {
 };
 st_echarts(
     options=option_2, height="600px")
+
+
+#-------------------------------
+ROOT_PATH = 'https://echarts.apache.org/examples';
+
+option = {
+  'backgroundColor': '#000',
+  'globe': {
+    'baseTexture': ROOT_PATH + '/data-gl/asset/earth.jpg',
+    'heightTexture': ROOT_PATH + '/data-gl/asset/bathymetry_bw_composite_4k.jpg',
+    'displacementScale': 0.1,
+    'shading': 'lambert',
+    'environment': ROOT_PATH + '/data-gl/asset/starfield.jpg',
+    'light': {
+      'ambient': {
+        'intensity': 0.1
+      },
+      'main': {
+        'intensity': 1.5
+      }
+    },
+    'layers': [
+      {
+        'type': 'blend',
+        'blendTo': 'emission',
+        'texture': ROOT_PATH + '/data-gl/asset/night.jpg'
+      },
+      {
+        'type': 'overlay',
+        'texture': ROOT_PATH + '/data-gl/asset/clouds.png',
+        'shading': 'lambert',
+        'distance': 5
+      }
+    ]
+  },
+  'series': []
+};
+
+st_echarts(
+    options=option, height="600px")
+
+
