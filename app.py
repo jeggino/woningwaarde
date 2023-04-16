21f99ac10b4ff8e8199fb7e93797790d74a8259f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-16 11:31:30 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import datetime
+from datetime import date,datetime
+import altair as alt
+import plotly.express as px
+import pydeck as pdk
+
+
+# -------------------------------------------------------
+st.set_page_config(
+    page_title="Amterdam woon[plaan",
+    page_icon="🏠",
+    layout="wide",
+)
+
+
+# -------------------------------------------------------
+@st.cache_data() 
+def get_data():
+    df_raw = gpd.read_file('https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=WONINGBOUWPLANNEN&THEMA=woningbouwplannen')
+    df_raw = df_raw[df_raw.Start_bouw!=0]
+    return df_raw
+
+
+# -------------------------------------------------------
+df = get_data()
+
+
+# -------------------------------------------------------
+sidebar = st.sidebar
