202c1b8296c2f2c0e00b02c144aa8f1710e37936
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-03 17:37:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -550,8 +550,6 @@ with row_2_2:
 
 #-----------------------------------
 from st_on_hover_tabs import on_hover_tabs
-import streamlit as st
-st.set_page_config(layout="wide")
 
 st.header("Custom tab component for on-hover navigation bar")
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
