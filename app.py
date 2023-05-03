91e380f24cee01fdec2bcc179e2729e9f0bcd3e8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-03 17:55:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -559,8 +559,8 @@ with st.sidebar:
                              iconName=['dashboard', 'money', 'economy'],
                              styles = {'navtab': {'background-color':'#111',
                                                   'color': '#818181',
-                                                  'font-size': '18px',
-                                                  'transition': '.3s',
+                                                  'font-size': '10px',
+                                                  'transition': '.1s',
                                                   'white-space': 'nowrap',
                                                   'text-transform': 'uppercase'},
                                        'tabOptionsStyle': {':hover :hover': {'color': 'red',
@@ -569,7 +569,7 @@ with st.sidebar:
                                                     'left':'7.5px',
                                                     'text-align': 'left'},
                                        'tabStyle' : {'list-style-type': 'none',
-                                                     'margin-bottom': '30px',
+                                                     'margin-bottom': '2px',
                                                      'padding-left': '30px'}},
                              key="1")
         
