1e6625b3d1007ce01cc496707cb8d61deac0de6c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-06 07:52:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,8 @@ def analysis_cluster():
 
 
     # -------------------------------------------------------
-    option_clusters = st.sidebar.number_input(f'The best number of cluster is {kl.elbow}, but you can play aroud',min_value=2, max_value=8,step=1,value=kl.elbow)
+    option_clusters = st.sidebar.number_input(f'The best number of cluster is {kl.elbow}, but you can play aroud',
+                                              min_value=2, max_value=8,step=1,value=kl.elbow)
 
     kmeans = cluster.KMeans(n_clusters=option_clusters,init="k-means++")
     kmeans = kmeans.fit(x_MinMax)
@@ -134,7 +135,7 @@ import seaborn as sns
 
 
 PALETTE = [ 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'crest', 'crest_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'flare', 'flare_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'icefire', 'icefire_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'mako', 'mako_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'rocket', 'rocket_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'vlag', 'vlag_r', 'winter', 'winter_r']
-option_palette = st.sidebar.selectbox('Palette',PALETTE)
+option_palette = st.sidebar.selectbox('Palette',PALETTE,index=4)
 palette = sns.color_palette(option_palette,n_colors=option_clusters)
 
 
@@ -217,7 +218,8 @@ from yellowbrick.classifier import ClassPredictionError
 
 
 # Create classification dataset
-X, y = analysis_cluster()[2], df_segmentation["Clusters"]
+X = analysis_cluster()[2]
+y = df_segmentation["Clusters"]
 
 # Perform 80/20 training/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
