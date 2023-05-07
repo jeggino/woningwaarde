6caea642bcc857913e730be48cf561eea1bfc81b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-07 07:21:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -167,9 +167,6 @@ colors = dict(zip(list(range(1,option_clusters+1)),
 df_segmentation['Color'] = df_segmentation['Clusters'].map(colors)
 df_segmentation['Color'] = df_segmentation["Color"].apply(lambda x: [round(i * 255) for i in x])
 
-def on_click(info):
-    print('Testing...')
-    st.write("TEST TEST")
 
 polygon_layer = pdk.Layer(
     'GeoJsonLayer',
@@ -184,7 +181,6 @@ polygon_layer = pdk.Layer(
     get_fill_color='Color',
     get_line_color=[255, 255, 255],
     pickable=True,
-    on_click=on_click
 )
 
 INITIAL_VIEW_STATE = pdk.ViewState(
@@ -213,15 +209,10 @@ with right:
     st.pydeck_chart(pydeck_obj=r, use_container_width=True)
     
     def filter_by_viewport(widget_instance, payload):
-        try:
-            west_lng, north_lat = payload['data']['nw']
-            east_lng, south_lat = payload['data']['se']
-            filtered_df = df[df.apply(lambda row: filter_by_bbox(row, west_lng, east_lng, north_lat, south_lat), axis=1)]
-            text.value = 'Points in viewport: %s' % int(filtered_df.count()['lng'])
-        except Exception as e:
-            text.value = 'Error: %s' % e
+        
+        return payload['data']
             
-    st.json(r.deck_widget.on_click(filter_by_viewport), expanded=True)
+    st.write(r.deck_widget.on_click(filter_by_viewport))
        
     
 #-----------------------------
