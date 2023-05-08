8a04c2fb5b05110d0c1e193439ce4bb3c401b880
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-08 13:09:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -289,16 +289,33 @@ from streamlit_plotly_events import plotly_events
 import streamlit as st
 from streamlit_plotly_events import plotly_events
 
-import pandas as pd
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-
+import plotly.io as pio
+import streamlit as st
 import plotly.express as px
 
-fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-selected_points = plotly_events(fig, click_event=False, hover_event=True)
+pio.templates.default = "plotly"
+
+df = px.data.tips()
+fig = px.scatter(
+    df,
+    x="total_bill",
+    y="tip",
+    color="day",
+    color_discrete_sequence=[
+        "#0068c9",
+        "#83c9ff",
+        "#ff2b2b",
+        "#ffabab",
+        "#29b09d",
+        "#7defa1",
+        "#ff8700",
+        "#ffd16a",
+        "#6d3fc0",
+        "#d5dae5",
+    ],
+    title="streamlit colors",
+)
+st.plotly_chart(fig)
 
 
 # df_folium = df_segmentation
