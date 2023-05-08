e4ba19bb92e774996420154d8173db66b37ff7ed
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-08 13:21:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -292,10 +292,46 @@ from streamlit_plotly_events import plotly_events
 import streamlit as st
 from streamlit_plotly_events import plotly_events
 
-df = px.data.gapminder().query("year==2007")
-fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
-                     hover_name="country", size="pop",
-                     projection="natural earth")
+import plotly.graph_objects as go
+
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+df.head()
+
+df['text'] = df['name'] + '<br>Population ' + (df['pop']/1e6).astype(str)+' million'
+limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
+cities = []
+scale = 5000
+
+fig = go.Figure()
+
+for i in range(len(limits)):
+    lim = limits[i]
+    df_sub = df[lim[0]:lim[1]]
+    fig.add_trace(go.Scattergeo(
+        locationmode = 'USA-states',
+        lon = df_sub['lon'],
+        lat = df_sub['lat'],
+        text = df_sub['text'],
+        marker = dict(
+            size = df_sub['pop']/scale,
+            color = colors[i],
+            line_color='rgb(40,40,40)',
+            line_width=0.5,
+            sizemode = 'area'
+        ),
+        name = '{0} - {1}'.format(lim[0],lim[1])))
+
+fig.update_layout(
+        title_text = '2014 US city populations<br>(Click legend to toggle traces)',
+        showlegend = True,
+        geo = dict(
+            scope = 'usa',
+            landcolor = 'rgb(217, 217, 217)',
+        )
+    )
 
 selected_points = plotly_events(fig, click_event=True, hover_event=False)
 st.dataframe(df)
