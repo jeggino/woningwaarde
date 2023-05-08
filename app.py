1cedcfda80ce5efef08dec9ad35375a7e7b689b7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-08 12:52:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -289,18 +289,16 @@ from streamlit_plotly_events import plotly_events
 import streamlit as st
 from streamlit_plotly_events import plotly_events
 
-# Writes a component similar to st.write()
-fig = px.line(x=[1], y=[1])
-selected_points = plotly_events(fig)
+import pandas as pd
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
 
-# Can write inside of things using with!
-with st.beta_expander('Plot'):
-    fig = px.line(x=[1], y=[1])
-    selected_points = plotly_events(fig)
+import plotly.express as px
 
-# Select other Plotly events by specifying kwargs
-fig = px.line(x=[1], y=[1])
-selected_points = plotly_events(fig, click_event=False, hover_event=True)
+fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+selected_points = plotly_events(fig, click_event=True, hover_event=True)
 
 
 # df_folium = df_segmentation
