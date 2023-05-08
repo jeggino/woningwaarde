916238cdddcb13c2ebd2421d5b4fcff9a1331dc0
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-08 19:16:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -454,7 +454,6 @@ st.write(df_table[df_table.index==selected_points_2[0]["pointNumber"]])
 
 #-------------------
 import plotly.express as px
-px.set_mapbox_access_token(open(".mapbox_token").read())
 df = px.data.carshare()
 fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon",     color="peak_hour", size="car_hours",
                   color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
