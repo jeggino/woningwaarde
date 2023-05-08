621db6dd703eb403cefeca21b30e0e54b1fde675
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-08 14:07:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -396,7 +396,7 @@ layer = pdk.Layer(
 view_state = pdk.ViewState(latitude=37.7749295, longitude=-122.4194155, zoom=10, bearing=0, pitch=0)
 
 # Render
-r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{name}\n{address}"},map_style='mapbox://styles/mapbox/outdoors-v12')
+r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{namej}\n{address}"},map_style='mapbox://styles/mapbox/outdoors-v12')
 def filter_by_viewport(widget_instance, payload):
     return payload['data']['exits_radius']
         
