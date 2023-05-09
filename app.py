c6c46da646b2f542960ff7cab565f3cc31d28899
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-09 07:43:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -531,6 +531,25 @@ fig4 = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
 fig4
 
 
+#---------------------------
+import plotly.graph_objects as go
+
+fig5 = go.Figure(go.Indicator(
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    value = 450,
+    mode = "gauge+number+delta",
+    title = {'text': "Speed"},
+    delta = {'reference': 380},
+    gauge = {'axis': {'range': [None, 500]},
+             'steps' : [
+                 {'range': [0, 250], 'color': "green"},
+                 {'range': [250, 400], 'color': "yellow"},
+                {'range': [400, 500], 'color': "red"}],
+             'threshold' : {'line': {'color': "white", 'width': 4}, 'thickness': 0.75, 'value': 490}}))
+
+fig5
+
+
 
 
 
