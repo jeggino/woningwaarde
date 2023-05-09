b8ab3373d64c6049f3d9cc3c10f8ec6254172cfb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-09 07:32:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -525,10 +525,10 @@ st.write(df_3[df_3.index==selected_points_4[0]["pointNumber"]])
 #---------------------
 import plotly.graph_objects as go
 
-df_3
 fig4 = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
                  cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))
                      ])
+fig4
 
 
 
