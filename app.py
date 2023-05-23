abb14a1df069d13b2e1591fd7ac736fdfe1c8aab
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-23 12:37:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,31 @@ st.set_page_config(
     layout="wide",
 )
 
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                       .format(user="root",
+                               pw="Platinum79",
+                               db="ebird"))
+
+COLUMNS = ['comName', 'date', 'lat', 'lng', 'locId', 'sciName', 'subId']
+
+df_old = pd.read_sql("SELECT * FROM df",con=engine,columns=columns)[columns]
+
+records = get_observations(API_KEY, COUNTRIES,back=BACK)
+df_ebird = pd.DataFrame(records)
+df_ebird['date'] = df_ebird.obsDt.str.split(" ",expand=True)[0]
+df_ebird = df_ebird[columns]
+
+df_updated = pd.concat([df_ebird,df_old],axis=0)
+df_updated[['lat', 'lng']] = df_updated[['lat', 'lng']].astype("float")
+df_updated.drop_duplicates(inplace=True)
+df_updated.reset_index(drop=True,inplace=True)
+
+df_updated.to_sql(con=engine, name='df', if_exists='replace')
+
+st.dataframe(df_updated)
+
 
 
 # left, right = st.columns([2,3],gap="large")
