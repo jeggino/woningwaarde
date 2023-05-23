c3898d1a756a1c291e3e821ce197b7fee7b9e595
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-23 14:00:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,24 +15,11 @@ st.write(st.secrets["connections"]["mysql"])
 st.write(st.secrets)
 
 
-import streamlit as st
+# import streamlit as st
 
 conn = st.experimental_connection("mysql", type="sql")
 
-if st.button("Create table"):
-    with conn.session as s:
-        st.markdown(f"Note that `s` is a `{type(s)}`")
-        s.execute("CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);")
-        s.execute("DELETE FROM pet_owners;")
-        pet_owners = {"jerry": "fish", "barbara": "cat", "alex": "puppy"}
-        for k in pet_owners:
-            s.execute(
-                "INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);",
-                params=dict(owner=k, pet=pet_owners[k]),
-            )
-        s.commit()
-
-df = conn.query("select * from pet_owners", ttl=600)
+df = conn.query("select * from df", ttl=600)
 df
 
 # left, right = st.columns([2,3],gap="large")
