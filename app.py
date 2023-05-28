04593e735496c22c3ce848aa00e942a7cb22a347
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-28 10:57:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,10 @@
 # streamlit_app.py
 
 import streamlit as st
-import psycopg2
+import pandas as pd
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT * from eBird_df;")
-
-# # Print results.
-# for row in rows:
-#     st.write(f"{row[0]} has a :{row[1]}:")
+df = pd.read_file(r"C:\Users\Luigi\OneDrive\Desktop\R\datasets\ebird.csv")
+st.dataframe(df)
     
     
     
